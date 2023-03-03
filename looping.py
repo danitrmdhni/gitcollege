f516a35cd78af1e77327b99ab23280fe9a699094
danitrmdhni <danitramadhani25@gmail.com>
2023-03-03 15:08:56 +0700

    Mengubah source code ganjil genap dengan if else

--- looping.py
+++ looping.py
@@ -1,3 +1,11 @@
 # 1. Membuat Looping 1-100
 for i in range(1, 101):
     print(i)
+
+# 2. Mengecek apakah bilangan tersebut adalah genap / ganjil. Jika genap maka jalankan dan jika ganjil maka skip
+# Menggunakan if else
+for i in range(1, 101):
+    if i % 2 == 0:
+        continue
+    else:
+        print(i)
